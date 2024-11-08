a48492155d84c928c150a0dad5161556a1f9c3e1
Gudhe Jagadeesh <jagadeesh8808@gmail.com>
2024-11-08 17:15:18 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//signup and login navigation
 function signup(event) {
     event.preventDefault();
 
@@ -20,13 +21,60 @@ function login(event) {
     if (username === storedUsername && password === storedPassword) {
       alert('Login successful!');
       window.location.href = 'welcome.html';
-    } else {
+    } 
+    else {
       alert('Invalid username or password.');
     }
   }
 
 
 
+//image changing 
+  const images = [
+    "images/hero-image1.jpg",
+    "images/hero-image2.jpg",
+    "images/hero-image3.jpg"
+  ];
+  let currentIndex = 0;
+  function changeImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.getElementById("slider-image").src = images[currentIndex];
+  }
+  setInterval(changeImage, 1000);
+  
+
+
+  //stats numbers counting 
+  const counters = document.querySelectorAll('.counter');
+  const speed = 500; 
+  
+  counters.forEach(counter => {
+    function updateCount() {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+      const increment = target / speed; //here increment is calculate
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 30);
+      } 
+      else {
+        counter.innerText = target; 
+      }
+    }
+    
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          updateCount();
+          observer.unobserve(counter); 
+        }
+      });
+    }, { threshold: 0.5 });
+    
+    observer.observe(counter);
+  });
+  
+  
 
 
   //navigate to start free trail to signup page
@@ -40,6 +88,7 @@ function login(event) {
 document.getElementById("discover-more-btn").addEventListener("click", function() {
     window.open("https://www.investopedia.com/articles/00/082100.asp", "_blank");
   });
+
 
 
 //overlaying the team images //
